b5420c45917546c4f2f581956dc541a052674187
vikash kumawat <vickykumawat1221@gmail.com>
2026-05-23 09:37:10 +0530

    Update expertRoutes.js
    Added expert rating and reviews

--- backend/backend/routes/expertRoutes.js
+++ backend/backend/routes/expertRoutes.js
@@ -2,20 +2,49 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
-const ExpertSchema =
-new mongoose.Schema({
-    name: String,
-    profession: String,
-    experience: String,
-    phone: String,
-    email: String,
-    languages: String
+// const ExpertSchema =
+// new mongoose.Schema({
+//     name: String,
+//     profession: String,
+//     experience: String,
+//     phone: String,
+//     email: String,
+//     languages: String
+// });
+
+
+const expertSchema = new mongoose.Schema({
+  name: String,
+  profession: String,
+  experience: String,
+  phone: String,
+  email: String,
+  languages: String,
+  rating: {
+    type: Number,
+    default: 0
+  },
+  reviews: [
+    {
+      userName: String,
+      comment: String,
+      stars: Number,
+      date: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ]
 });
+
+
+
+
 
 const Expert =
 mongoose.model(
     "Expert",
-    ExpertSchema
+    expertSchema
 );
 
 // Save Expert
@@ -49,5 +78,39 @@ router.get(
 
     }
 );
+
+//new changes
+router.post("/:id/review", async (req, res) => {
+  try {
+    const { userName, comment, stars } = req.body;
+
+    const expert = await Expert.findById(req.params.id);
+
+    expert.reviews.push({
+      userName,
+      comment,
+      stars
+    });
+
+    const totalStars = expert.reviews.reduce(
+      (sum, review) => sum + review.stars,
+      0
+    );
+
+    expert.rating = totalStars / expert.reviews.length;
+
+    await expert.save();
+
+    res.json(expert);
+
+  } catch (error) {
+    res.status(500).json({
+      error: error.message
+    });
+  }
+});
+
+
+
 
 module.exports = router;
